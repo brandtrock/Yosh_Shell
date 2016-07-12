a2e6f6ab4e7ac4cc8651290ac629a54d6167d547
Brandt Jorgensen <bmjorgensen@outlook.com>
2016-07-12 06:49:46 -0500

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,39 @@
+import os
+import shlex
+import sys
+
+SHELL_STATUS_RUN = 1
+SHELL_STATUS_STOP = 0
+
+def execute(cmd_tokens):
+    # Execute command
+    os.execvp(cmd_tokens[0], cmd_tokens)
+
+    # Return status indicating to wait for next command in shell_loop
+    return SHELL_STATUS_RUN
+
+def tokenize(string):
+    return shlex.split(string)
+
+def shell_loop():
+    status = SHELL_STATUS_RUN
+
+    while status == SHELL_STATUS_RUN:
+        # Display a command prompt
+        sys.stdout.write('> ')
+        sys.stdout.flush()
+
+        # Read command input
+        cmd = sys.stdin.readline()
+
+        # Tokenize the command input
+        cmd_tokens = tokenize(cmd)
+
+        # Execute the command and retrieve new status
+        status = execute(cmd_tokens)
+
+def main():
+    shell_loop()
+
+if __name__ == "__main__":
+    main()
